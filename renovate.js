bf9df20f5517301885828f5e7a3d5108fafc7823
Tijmen Bruggeman <tijmen.bruggeman@ster.nl>
2026-01-02 15:42:03 +0100

    renovate updates

--- renovate.js
+++ renovate.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: ["config:best-practices"],
+  extends: ["config:recommended"],
+  minimumReleaseAge: "14 days",
   hostRules: [
     {
       hostType: "npm",
@@ -15,60 +16,6 @@ module.exports = {
     },
   ],
   packageRules: [
-    {
-      matchDatasources: ["npm"],
-      minimumReleaseAge: "14 days",
-    },
-    {
-      description: "Only allow minor standard upgrades",
-      matchUpdateTypes: ["major", "patch", "pin"],
-      enabled: false,
-    },
-    {
-      groupName: "@lingui",
-      matchPackageNames: ["@lingui/**"],
-    },
-    {
-      groupName: "Microsoft.EntityFrameworkCore",
-      matchPackageNames: [
-        "Microsoft.EntityFrameworkCore.**",
-        "Microsoft.EntityFrameworkCore",
-      ],
-    },
-    {
-      groupName: "Microsoft.AspNetCore",
-      matchPackageNames: ["Microsoft.AspNetCore.**"],
-    },
-    {
-      groupName: "Microsoft.FeatureManagement",
-      matchPackageNames: [
-        "Microsoft.FeatureManagement.**",
-        "Microsoft.FeatureManagement",
-      ],
-    },
-    {
-      groupName: "@storybook",
-      matchPackageNames: [
-        "@storybook/**",
-        "storybook",
-        "eslint-plugin-storybook",
-      ],
-    },
-    {
-      groupName: "@microsoft",
-      matchPackageNames: [
-        "@microsoft/applicationinsights-web",
-        "@microsoft/applicationinsights-clickanalytics-js",
-      ],
-    },
-    {
-      groupName: "ag-grid",
-      matchPackageNames: ["ag-grid-**", "@ag-grid-**"],
-    },
-    {
-      groupName: "@azure",
-      matchPackageNames: ["@azure/**"],
-    },
     {
       groupName: "React",
       matchPackageNames: ["react", "react-dom"],
@@ -88,11 +35,5 @@ module.exports = {
         "Microsoft.Identity.Web.**",
       ],
     },
-  ],
-  prHourlyLimit: 10,
-  platformAutomerge: true,
-  rangeStrategy: "bump",
-  dependencyDashboard: true,
-  enabledManagers: ["npm", "nuget"], // not updating the bicep files for now.
-  repositories: ["StarLight 2.0/StarLight2"],
+  ]
 };
